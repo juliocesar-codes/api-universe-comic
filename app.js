9deb7ea72910199c612a2c291d30fcfb101a0992
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2025-09-29 15:56:10 -0300

    Imagens da API aparecendo na página

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+async function buscarImagem(personagem) {
+    const url = `https://www.superheroapi.com/api.php/0092bd3d8f30eabe0f235a3da1fc3470/search/${personagem}`;
+    const response = await fetch(url);
+    const imagens = await response.json();    
+    imagens.results.forEach(item =>{
+        console.log(item.image.url) 
+    })
+
+    return imagens.results
+    
+}
+
+async function criarImagem(){
+
+    const main = document.getElementById('main')
+    main.textContent = ''
+    const heroi = document.getElementById( 'inputHeroi').value
+
+    const herois = await buscarImagem(heroi)
+    
+    herois.forEach((url)=>{
+        const img = document.createElement('img')
+        // Usando proxy alternativo
+        img.src = 'https://corsproxy.io/?' + url.image.url
+        main.appendChild(img)
+    })
+}
+
+const btn = function(event){
+    if (event.key === 'Enter'){
+        criarImagem()
+    }
+}
