96100b8bfb5fce998f9b5ffcdf7fae68e8c86887
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2025-10-09 16:56:49 -0300

    Inicio da edição da página de detalhes

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 async function buscarImagem(personagem) {
     const url = `https://www.superheroapi.com/api.php/0092bd3d8f30eabe0f235a3da1fc3470/search/${personagem}`;
     const response = await fetch(url);
-    const imagens = await response.json();    
-    imagens.results.forEach(item =>{
-    })
+    const imagens = await response.json();
+    imagens.results.forEach(item => {})
 
     return imagens.results
-    
+
 }
 
-async function detalheHeroi(url){
+async function detalheHeroi(url) {
     const main = document.getElementById('main')
     main.textContent = ''
+
+    let divDetalhes = document.createElement('div')
+    divDetalhes.className = 'divDetalhes'
 
     const h2 = document.createElement('h2')
     h2.textContent = "Página de Detalhes"
 
     console.log(url)
+    // console.log(url.connections.group-affiliation)
 
     const nome = document.createElement('p')
     nome.textContent = url.name
@@ -27,26 +30,28 @@ async function detalheHeroi(url){
     image.src = 'https://corsproxy.io/?' + url.image.url
 
     main.appendChild(h2)
-    main.appendChild(image)
-    main.appendChild(nome)
+    divDetalhes.appendChild(image)
+    divDetalhes.appendChild(nome)
+    main.appendChild(divDetalhes)
+
 }
 
-async function criarImagem(){
+async function criarImagem() {
     main.textContent = ''
-    
-    const heroi = document.getElementById( 'inputHeroi').value
+
+    const heroi = document.getElementById('inputHeroi').value
 
     const herois = await buscarImagem(heroi)
 
     const div = document.createElement('div')
-    
-    herois.forEach((url)=>{
+
+    herois.forEach((url) => {
         const img = document.createElement('img')
-        // Usando proxy alternativo
+            // Usando proxy alternativo
         img.src = 'https://corsproxy.io/?' + url.image.url
 
         const a = document.createElement('a')
-        a.addEventListener('click', function(){
+        a.addEventListener('click', function() {
 
             detalheHeroi(url);
         })
@@ -54,14 +59,13 @@ async function criarImagem(){
         main.appendChild(div)
         div.appendChild(a)
         a.appendChild(img)
-        
+
     })
 
 }
 
-const btn = function(event){
-    if (event.key === 'Enter'){
+const btn = function(event) {
+    if (event.key === 'Enter') {
         criarImagem()
     }
 }
-
