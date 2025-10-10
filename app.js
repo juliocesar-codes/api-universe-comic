17e5f048e0a6a7e4bd3292d277ae1be737a1e596
juliocesar-codes <cesar-pb@hotmail.com>
2025-10-10 11:42:56 -0300

    Update

--- app.js
+++ app.js
@@ -1,42 +1,73 @@
 'use strict'
 
+
+
 async function buscarImagem(personagem) {
     const url = `https://www.superheroapi.com/api.php/0092bd3d8f30eabe0f235a3da1fc3470/search/${personagem}`;
     const response = await fetch(url);
     const imagens = await response.json();
-    imagens.results.forEach(item => {})
+    // imagens.results.forEach(item => {})
 
     return imagens.results
 
 }
 
-async function detalheHeroi(url) {
+async function afiliacaoHeroi() {
+
+    main.textContent = ''
+}
+
+async function detalheHeroi(heroi) {
+
+
     const main = document.getElementById('main')
     main.textContent = ''
 
     let divDetalhes = document.createElement('div')
-    divDetalhes.className = 'divDetalhes'
+    divDetalhes.className = 'pagina-detalhes'
+
+    let detalhesText = document.createElement('div')
+    divDetalhes.className = 'detalhesText'
 
     const h2 = document.createElement('h2')
     h2.textContent = "Página de Detalhes"
 
-    console.log(url)
-    // console.log(url.connections.group-affiliation)
+    const afiliacoesString = heroi.connections['group-affiliation']
 
-    const nome = document.createElement('p')
-    nome.textContent = url.name
+    const afiliacoesArray = afiliacoesString.split(',').map(item => item.trim())
+
+    const nome = document.createElement('h3')
+    nome.textContent = heroi.name
 
     const image = document.createElement('img')
-    image.src = 'https://corsproxy.io/?' + url.image.url
+    image.src = 'https://corsproxy.io/?' + heroi.image.url
 
     main.appendChild(h2)
     divDetalhes.appendChild(image)
     divDetalhes.appendChild(nome)
     main.appendChild(divDetalhes)
 
-}
+    afiliacoesArray.forEach(afiliacao => {
+        const p = document.createElement('p')
+        p.textContent = afiliacao
 
+
+        const a = document.createElement('a')
+        a.addEventListener('click', function() {
+            afiliacaoHeroi()
+
+
+        })
+        divDetalhes.appendChild(a)
+        a.appendChild(p)
+    })
+
+
+
+
+}
 async function criarImagem() {
+    const main = document.getElementById('main')
     main.textContent = ''
 
     const heroi = document.getElementById('inputHeroi').value
