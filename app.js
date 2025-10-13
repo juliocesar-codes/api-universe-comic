4ea8f9eea9db7acf590583e7475b574257b17969
juliocesar-codes <cesar-pb@hotmail.com>
2025-10-13 18:28:48 -0300

    Edições Pendentes

--- app.js
+++ app.js
@@ -12,9 +12,46 @@ async function buscarImagem(personagem) {
 
 }
 
-async function afiliacaoHeroi() {
+async function afiliacaoHeroi(afiliacao) {
+
+    const main = document.getElementById('main')
+
+    const url2 = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`
+    const response = await fetch(url2);
+    const imagens = await response.json();
+
 
     main.textContent = ''
+
+    console.log(afiliacao)
+
+    console.log(imagens)
+
+    imagens.forEach(item => {
+        // console.log(item.connections.groupAffiliation)
+        if (item.connections.groupAffiliation.includes(afiliacao)) {
+            console.log(item.name)
+
+            const div = document.createElement('div')
+
+            const img = document.createElement('img')
+                // Usando proxy alternativo
+            img.src = 'https://corsproxy.io/?' + item.images.lg
+
+            const heroi = item
+
+            const a = document.createElement('a')
+            a.addEventListener('click', function() {
+
+                detalheHeroi(heroi);
+            })
+
+            main.appendChild(div)
+            div.appendChild(a)
+            a.appendChild(img)
+        }
+
+    })
 }
 
 async function detalheHeroi(heroi) {
@@ -32,15 +69,20 @@ async function detalheHeroi(heroi) {
     const h2 = document.createElement('h2')
     h2.textContent = "Página de Detalhes"
 
-    const afiliacoesString = heroi.connections['group-affiliation']
+    const afiliacoesString = heroi.connections.groupAffiliation || heroi.connections['group-affiliation'];
+
+    const urlImagem = heroi.image ? heroi.image.url : heroi.images.lg;
+    // Esta linha é equivalente a um if/else porém mais conciso
 
     const afiliacoesArray = afiliacoesString.split(',').map(item => item.trim())
+
+    console.log(afiliacoesArray)
 
     const nome = document.createElement('h3')
     nome.textContent = heroi.name
 
     const image = document.createElement('img')
-    image.src = 'https://corsproxy.io/?' + heroi.image.url
+    image.src = 'https://corsproxy.io/?' + urlImagem
 
     main.appendChild(h2)
     divDetalhes.appendChild(image)
@@ -54,16 +96,13 @@ async function detalheHeroi(heroi) {
 
         const a = document.createElement('a')
         a.addEventListener('click', function() {
-            afiliacaoHeroi()
+            afiliacaoHeroi(afiliacao)
 
 
         })
         divDetalhes.appendChild(a)
         a.appendChild(p)
     })
-
-
-
 
 }
 async function criarImagem() {
